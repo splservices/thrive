36b8bad672ee4c46653006dae713e353887dbee6
Pradeep Kumar <pradeepkumar@Pradeeps-MacBook-Air.local>
2019-03-21 19:36:23 +0530

    celebrate

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,7 +4,7 @@ const loginValidation = require('../../validations/api.validation');
 const {loginUser, registerUser} = require('../../controllers/user.controller');
 const  { celebrate, Joi }  = require('celebrate');
 
-router.post('/login', loginUser);
+router.post('/login', celebrate(loginValidation.loginDetail), loginUser);
 router.post('/register', registerUser);
 
 module.exports = router;
