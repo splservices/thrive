869b08b27f6b899717b6307f9f6cebce7022db1a
Pradeep Kumar <pradeepkumar@Pradeeps-MacBook-Air.local>
2019-03-30 11:30:44 +0530

    make it better

--- app.js
+++ app.js
@@ -1,52 +1,11 @@
 
 
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const cookieSession = require('cookie-session');
-const bodyParser = require('body-parser');
-const boom = require('express-boom');
-const session = require('express-session');
-const helmet = require('helmet');
-const cors = require('cors');
-const fs = require('fs');
-const path = require('path');
-//const { errors } = require('celebrate');
-const passport = require('passport');
 
-const index = require('./routes/index');
 
 const app = express();
 
-app.use(bodyParser.json({limit:'1mb'}));
-app.use(bodyParser.urlencoded({limit:'1mb',extended:true}));
-app.use(cookieParser());
-app.use(cookieSession({
-    maxAge:24*60*60*1000,
-    keys:['sdfgsdfg']
-}))
-// app.use(session({name:'thrive', secret:'thrive'}));
-app.use(helmet());
-app.use(boom());
-app.use(cors());
-
-require('./config/passport')(passport); // pass passport for configuration
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-app.use(express.static('build'));
-
-
-
-app.use('/api', index);
-app.use('*',(req, res, next)=>{
-    var output = fs.readFileSync(__dirname + '/build/index.html');
-    res.type('html').send(output);
-    //next();
-
-});
-
+console.log('app is working fine')
 
 
 //app.use(errors());
