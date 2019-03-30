6b2d46e5aa665b053af4bb7bf9692762142750a6
Pradeep Kumar <pradeepkumar@Pradeeps-MacBook-Air.local>
2019-03-30 11:38:42 +0530

    it was nvm issue

--- app.js
+++ app.js
@@ -15,17 +15,38 @@ const passport = require('passport');
 
 const index = require('./routes/index');
 
-
 const app = express();
 
-// app.use(bodyParser.json({limit:'1mb'}));
-// app.use(bodyParser.urlencoded({limit:'1mb',extended:true}));
-// app.use(cookieParser());
-// app.use(helmet());
-// app.use(boom());
-// app.use(cors());
+app.use(bodyParser.json({limit:'1mb'}));
+app.use(bodyParser.urlencoded({limit:'1mb',extended:true}));
+app.use(cookieParser());
+app.use(cookieSession({
+    maxAge:24*60*60*1000,
+    keys:['sdfgsdfg']
+}));
+// app.use(session({name:'thrive', secret:'thrive'}));
+app.use(helmet());
+app.use(boom());
+app.use(cors());
 
-console.log('app is working fine')
+require('./config/passport')(passport); // pass passport for configuration
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(express.static('build'));
+
+
+
+app.use('/api', index);
+app.use('*',(req, res, next)=>{
+    var output = fs.readFileSync(__dirname + '/build/index.html');
+    res.type('html').send(output);
+    //next();
+
+});
+
 
 
 //app.use(errors());
