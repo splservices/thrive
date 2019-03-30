2426d571db62b946a84e26489974606b72abab0c
Pradeep Kumar <pradeepkumar@Pradeeps-MacBook-Air.local>
2019-03-30 11:32:23 +0530

    make it better

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 
 
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
+const boom = require('express-boom');
+const session = require('express-session');
+const helmet = require('helmet');
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+//const { errors } = require('celebrate');
+const passport = require('passport');
+
+const index = require('./routes/index');
 
 
 const app = express();
+
+app.use(bodyParser.json({limit:'1mb'}));
+app.use(bodyParser.urlencoded({limit:'1mb',extended:true}));
+app.use(cookieParser());
+app.use(helmet());
+app.use(boom());
+app.use(cors());
 
 console.log('app is working fine')
 
