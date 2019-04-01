e5086140dd447256817bd35f4d6193f979ff8c30
Pradeep <pradeep20898@gmail.com>
2019-04-01 21:57:29 +0530

    ui build

---
+++ build/precache-manifest.dea299c616de2ebd80ffa45e95097561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "e51b28056ac5714d43f6",
+    "url": "/static/js/main.7f6cd8fe.chunk.js"
+  },
+  {
+    "revision": "9fddb1d59d9f95709500",
+    "url": "/static/js/2.1a03f944.chunk.js"
+  },
+  {
+    "revision": "e51b28056ac5714d43f6",
+    "url": "/static/css/main.16a636da.chunk.css"
+  },
+  {
+    "revision": "d40549e9fd0ce1a043320db22d061057",
+    "url": "/index.html"
+  }
+];
